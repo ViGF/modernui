b579c5130f4ac3f1dc5439ed5e3d584d3a14baa2
ViGF <vitoriogdef@gmail.com>
2022-03-06 17:48:18 -0300

    Configuração inicial e criação da tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,2 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+export function Home() {
+    return (
+        <Container>
+            <Text>Home</Text>
+        </Container>
+    )
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+export default {
+    COLORS: {
+
+    },
+
+    FONTS: {
+
+    }
+}
